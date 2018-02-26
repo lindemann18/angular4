2eaf3eda2eaf7fa65edab1890f4f9be38f1f3ad8
jesus <jibanez@tacitknowledge.com>
2018-02-26 17:00:06 -0600

    ngfor

--- quickstart/src/app/empleado/empleado.component.ts
+++ quickstart/src/app/empleado/empleado.component.ts
@@ -11,17 +11,24 @@ export class EmpleadoComponent  {
   public titulo:string = "holis";
   public empleado:Empleado;
   public empleados:Array<Empleado>;
+  public trabajador_externo:boolean;
 
   constructor() {
     this.empleado = new Empleado("chuvakovich",29, "Associate Engineer",true);
     this.empleados = [
       new Empleado("Lizbeth",29, "chef",true),
       new Empleado("Vera",29, "Designer",true),
-      new Empleado("Alissa",29, "Support",true),
+      new Empleado("Alissa",29, "Support",false),
     ];
+    this.trabajador_externo = false;
   }
 
   ngOnInit() {
 
   }
+
+  cambiarExterno() {
+    this.trabajador_externo = !this.trabajador_externo;
+  }
+
 }
