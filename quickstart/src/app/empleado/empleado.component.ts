0bf9d83d24b33363d7122e89af8576a3ff75b438
Chuvakovich Zukovsky <juliusbelmont_1@hotmail.com>
2018-02-25 23:25:51 -0600

    clase 21 finalizada

--- quickstart/src/app/empleado/empleado.component.ts
+++ quickstart/src/app/empleado/empleado.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Empleado} from './empleado.model';
 
 @Component({
   selector: 'empleado',
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
 })
 
 export class EmpleadoComponent  {
-  public titulo:string = 'empleado';
+  public titulo:string = "holis";
+  public empleado:Empleado;
+  public empleados:Array<Empleado>;
 
+  constructor() {
+    this.empleado = new Empleado("chuvakovich",29, "Associate Engineer",true);
+    this.empleados = [
+      new Empleado("Lizbeth",29, "chef",true),
+      new Empleado("Vera",29, "Designer",true),
+      new Empleado("Alissa",29, "Support",true),
+    ];
+  }
+
+  ngOnInit() {
+
+  }
 }
--- quickstart/src/app/empleado/empleado.model.ts
+++ quickstart/src/app/empleado/empleado.model.ts
@@ -1,9 +1,11 @@
 export class Empleado {
-  public nombre;
-  public edad;
 
-  constructor(nombre, edad) {
-    this.nombre = nombre;
-    this.edad   = edad;
+  constructor(
+    public nombre:string,
+    public edad:number,
+    public cargo:string,
+    public contratado:boolean
+  ) {
+    
   }
 }
