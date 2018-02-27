37de4dee0faf4525692d43b7489d0b5c51ad0bcc
jesus <jibanez@tacitknowledge.com>
2018-02-27 14:20:53 -0600

    services

--- quickstart/src/app/app.module.ts
+++ quickstart/src/app/app.module.ts
@@ -2,19 +2,24 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule}     from '@angular/forms'; // two way data binding
 import {HttpModule}      from '@angular/http';
+import {routing, appRoutingProviders} from './app.routing';
 
 import { AppComponent }  from './app.component';
 import { FrutaComponent }  from './fruta/fruta.component';
 import { EmpleadoComponent }  from './empleado/empleado.component';
+import { ContactoComponent }  from './contacto/contacto.component';
+import { HomeComponent }  from './home/home.component';
 
 @NgModule({
-  imports:      [ BrowserModule , FormsModule, HttpModule],
+  imports:      [ BrowserModule , FormsModule, HttpModule,routing],
   declarations: [
     AppComponent,
     FrutaComponent,
     EmpleadoComponent,
+    HomeComponent,
+    ContactoComponent,
   ],
-  providers:[],
+  providers:[appRoutingProviders],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ quickstart/src/app/contacto/contacto.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'contacto',
+  templateUrl: './contacto.component.html',
+})
+export class ContactoComponent {
+  public titulo:string = 'Página Contacto';
+  public Parametro;
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) {
+
+  }
+
+
+  ngOnInit() {
+    this._route.params.forEach((params:Params)=>{
+      this.Parametro = params['page'];
+      console.log(params);
+    });
+  }
+
+  redirigir() {
+    this._router.navigate(['/contacto','perrilliou']);
+  }
+}
